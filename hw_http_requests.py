93f9499c993df9bc47997c60fe74e2de95a4c437
Natalia Larina <natashalarina777@mail.ru>
2020-09-02 20:34:07 +0300

    File added

--- hw_http_requests.py
+++ hw_http_requests.py
@@ -1,24 +1,18 @@
 import requests
 from pprint import pprint
 
-
-response = requests.get('https://superheroapi.com/api/2619421814940190/search/Hulk')
-response_1 = requests.get('https://superheroapi.com/api/2619421814940190/search/Thanos')
-response_2 = requests.get('https://superheroapi.com/api/2619421814940190/search/Captain_America')
-
-Hulk = response.json()
-Thanos = response_1.json()
-Captain_America = response_2.json()
-
 intelligence_dict = dict()
-def intelligence_hero(resp, name):
-    hero = resp['results']
-    for info in hero:
-        if info['name'] == name:
-            intelligence = info['powerstats']['intelligence']
-        # intelligence_list.append(intelligence)
-            intelligence_dict[info['name']] = int(intelligence)
-            return intelligence_dict
+
+def hero_intelligence(hero_lis):
+    for name in hero_lis:
+        resp = requests.get(f'https://superheroapi.com/api/2619421814940190/search/{name}')
+        resp_get = resp.json()
+        hero = resp_get['results']
+        for info in hero:
+            if info['name'] == name:
+                intelligence = info['powerstats']['intelligence']
+                intelligence_dict[info['name']] = int(intelligence)
+    return intelligence_dict
 
 def most_intelligence_hero(heroes):
     intel_point = []
@@ -27,12 +21,10 @@ def most_intelligence_hero(heroes):
     for key, value in heroes.items():
         if max(intel_point) == value:
             print(f'The most intelligence hero is {key}')
-
-
-
-intelligence_hero(Hulk, 'Hulk')
-intelligence_hero(Thanos, 'Thanos')
-intelligence_hero(Captain_America, 'Captain America')
-
+#
+#
+hero_list_data = ['Hulk','Captain America','Thanos']
+print(hero_intelligence(hero_list_data))
 most_intelligence_hero(intelligence_dict)
+
 

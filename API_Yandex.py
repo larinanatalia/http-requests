6d03942f44c6260a7b0af995cbb437afde4589fa
Natalia Larina <natashalarina777@mail.ru>
2020-09-02 18:45:42 +0300

    File added

--- API_Yandex.py
+++ API_Yandex.py
@@ -8,16 +8,21 @@ class YaUploader:
         import json
         path_list = self.file_path.split('/')
         pic_name = path_list[-1]
-        headers = {'Authorization': 'OAuth ++++++++++++++'}
+        headers = {'Authorization': 'OAuth ++++++++++++++++'}
         params = {'path': pic_name}
         resp = requests.get('https://cloud-api.yandex.net/v1/disk/resources/upload',
                                 headers=headers, params=params)
         resp_get = resp.json()
-        with open(self.file_path, 'rb') as f:
-            resp_put = requests.put(str(resp_get['href']), files={"file": f})
-        return 'File added'
+        if resp.status_code == 200:
+            with open(self.file_path, 'rb') as f:
+                resp_put = requests.put(str(resp_get['href']), files={"file": f})
+                print('File added')
+        else:
+            print(resp_get['message'])
 
 
+if __name__ == '__main__':
+    uploader = YaUploader('')
+    result = uploader.upload()
 
-naruto = YaUploader('C:/Users/79035/Desktop/py-33/hw-http/photos/naruto_1.jpg')
-naruto.upload()
+
